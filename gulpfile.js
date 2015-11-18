60b8375b795bc24486b2b3fdfeddbc6d0a757569
wandergis <763393390@qq.com>
2015-11-18 19:47:26 +0800

    minify html

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var minifyHtml = require("gulp-minify-html");
 var del = require('del');
 gulp.task('compress', function() {
 	gulp.src(['./js/**/*.js'])
 		.pipe(uglify())
 		.pipe(gulp.dest('./compressjs'));
 })
+gulp.task('minify-html', function() {
+	gulp.src('index.html') // 要压缩的html文件
+		.pipe(minifyHtml()) //压缩
+		.pipe(gulp.dest('dist'));
+});
 gulp.task('clean', function() {
 		del(['./js/compress/*'])
 	})
 	//默认任务
-gulp.task('default', ['compress'])
+gulp.task('default', ['minify-html'])
